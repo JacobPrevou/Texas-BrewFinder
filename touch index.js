05e05b8974ba82f24f586ed2882c990875c5c4e6
Jake <prevou15@gmail.com>
2021-11-01 19:09:37 -0500

    filtered cities grouped in a single list attribute

--- touch index.js
+++ touch index.js
@@ -28,14 +28,61 @@ async function fetchBreweries() {
 //Filter cities 
 async function renderCities() {
     let data = await fetchBreweries();
-    data.filter(brewery => {
-        console.log(brewery.city);
+    const select = document.getElementById('input')
+    const list = document.createElement('ul')
+    const option = document.createElement('OPTION')
+    
+    list.innerHTML = data.map(brewery => {
+        return brewery.city;
+        //option.innerHTML = brewery.city;
+
     });
+    option.appendChild(list);
+    select.appendChild(option)
+    //option.innerHTML = cityList;
+    //list.appendChild(option);
+    //option.innerHTML = `<option>${cityList}</option>`
+    //list.appendChild(option)
 }
 //Call renderCities()
 renderCities();
 
+//function renderCity(json) {
+//     document.querySelector('input')
+//      json.filter(brewery => {
+//         brewery.city;
+//      })
+//      const ul = document.createElement('ul')
+//      ul.innerHTML = `<ul>${brewery.city}</ul>`
+//      select.appendChild(ul)
+//}
 
+    // document.addEventListener('DOMContentLoaded', async () => {
+    //     let breweries = [];
+
+    //     try {
+    //         breweries = await fetchBreweries()
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+
+    //     console.log(breweries);
+    // });
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//     let breweries = [];
+//     const select = breweries.filter(obj => {
+//         console.log(obj.city);
+//         const list = document.getElementById('input')
+//         const ul = document.createElement('ul')
+//         ul.innerHTML = `<ul>${obj.city}</ul>`
+//         list.appendChild(ul)
+//     })
+// })
+
+
+
+//const selection = document.getElementById('input')
 
 
 
