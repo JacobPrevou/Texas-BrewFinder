e1f43c64e983d691676ee90f3f602f259c866798
Jake <prevou15@gmail.com>
2021-11-02 08:11:03 -0500

    Each object's city key set to select option

--- touch index.js
+++ touch index.js
@@ -23,33 +23,97 @@ async function fetchBreweries() {
     const data = await response.json();
     console.log(data); 
     var select = document.getElementById('input')
-    data.forEach(brewery => {
-        //Sort cities alphabetically
-        function sortArray(x, y){
-            if (x.city < y.city) {return -1;}
-            if (x.city > y.city) {return 1;}
-            return 0;
-        }
-        var sorted = data.sort(sortArray);
-        console.log(sorted);
 
-        var clean = sorted.filter((sorted, index, self) =>
-        index === self.findIndex((t) => (t.save === sorted.city)))
+//Set the city key for each object to a select option
+    for (var key in data) {
+    var brewery = data[key];
+    select.options[select.options.length] = new Option(brewery.city);
+    }
 
-        console.log(clean);
-        
-        // let uniqueChars = [...new Set(data.city)];
-        // console.log(uniqueChars);
-        
-        const option = document.createElement('option')
-        option.innerHTML = `<option>${brewery.city}</option>`
-        select.appendChild(option)
-    })
+
 
 }
 fetchBreweries()
 
+    // data.forEach(brewery => {
+    //     //Sort cities alphabetically
+    //     function sortArray(x, y){
+    //         if (x.city < y.city) {return -1;}
+    //         if (x.city > y.city) {return 1;}
+    //         return 0;
+    //     }
+    
+        
 
+        
+        // const option = document.createElement('option')
+        // option.innerHTML = `<option>${brewery.city}</option>`
+        // select.appendChild(option)
+    //})
+
+// var list = [];
+// var select = document.getElementById('input');
+// // let option;
+// // let dropdown = document.getElementById('input')
+// //filter cities for dropdown list
+// async function renderCities() {
+//     console.log(data)
+// //filter out duplicate locations
+// for (let i = 0; i < data.length; i++) {
+//     option = document.createElement('option');
+//     option.text = data[i].city;
+//     option.value = data[i].city;
+//     select.value = option;
+//   }
+// }
+
+// //Call renderCities()
+// renderCities();
+
+// var list = [];
+// var select = document.getElementById('input')
+// //filter cities for dropdown list
+// async function renderCities() {
+//     var data = await fetchBreweries();
+//     //const option = document.createElement('OPTION')
+    
+//     // const ddown = data.map(brewery => {
+//     //     list.push(brewery.city);
+//     // });
+
+//     // option.value = list
+//     // select.appendChild(option);
+
+//     // var list = document.createElement('li');
+//     // list.appendChild(document.createTextNode(brewery[i]));
+//     // cityList.appendChild(list);
+//     // select.appendChild(cityList);
+//     // })
+//     data.forEach(brewery => {
+//         const option = document.createElement('option')
+//         option.innerHTML = `<option>${brewery.city}</option>`
+//         select.appendChild(option)
+//     })
+
+//     return select
+//       //return list.push(ddown)
+
+// }
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//     let breweries = [];
+//     const select = breweries.filter(obj => {
+//         console.log(obj.city);
+//         const list = document.getElementById('input')
+//         const ul = document.createElement('ul')
+//         ul.innerHTML = `<ul>${obj.city}</ul>`
+//         list.appendChild(ul)
+//     })
+// })
+
+
+
+//const selection = document.getElementById('input')
 
 
 
