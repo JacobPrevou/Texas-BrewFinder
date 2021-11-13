671461c57817f93b8cc5eda98f6a5a661c42b9a2
Jake <prevou15@gmail.com>
2021-11-12 18:53:57 -0600

    Search bar and like button complete

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const searchWrapper = document.createElement('div');
 container.appendChild(searchWrapper);
 searchWrapper.id = "searchWrapper";
 
+
 //Create input box for searchBar
 const input = document.createElement('input');
 searchWrapper.appendChild(input);
@@ -17,8 +18,21 @@ input.type = "text";
 input.id = "searchBar";
 input.placeholder = "Type in a Texas city y'all!"
 
-//Ok you made the searchbar! Now connect with the API
 
+searchBar.addEventListener('input', event => {
+    event.preventDefault()
+    const searchString = event.target.value.toLowerCase();
+    let searchResult = filteredBreweries.filter(brewery => {
+        return (
+            brewery.city.toLowerCase().includes(searchString) 
+        );
+    });
+  displayBreweries(searchResult)
+});
+
+
+
+//Ok you made the searchbar! Now connect with the API
 function fetchBreweries() {
     fetch("https://api.openbrewerydb.org/breweries?by_state=texas")
     .then(res => res.json())
@@ -27,42 +41,46 @@ function fetchBreweries() {
         filteredBreweries = breweries
     })
 };
-fetchBreweries();
 
-let filteredBreweries = [];
+
 
 //Create a list element for the brewery "profiles"
-const breweryProfiles = document.createElement('ul');
-container.append(breweryProfiles);
-breweryProfiles.id = "profiles";
+const profiles = document.createElement('ul');
+container.append(profiles);
+profiles.id = "profiles";
+
 
 //Write a function that will map out the API data/designate the info you want for each brewery profile
 const displayBreweries = (breweries) => {
-    const htmlString = breweries
-        .map((brewery) => {
-            return `
-            <li class="brewery">
-                <h2>${brewery.name}</h2>
-                <p>Street: ${brewery.street}</p>
-                <p>City: ${brewery.city}</p>
-                <p>Zipcode: ${brewery.postal_code}
-            </li>
-        `;
-        })
-        .join('');
-    breweryProfiles.innerHTML = htmlString;
+    const htmlString = breweries.map((brewery) => {
+        return `
+        <li class="site">
+            <h2>${brewery.name}</h2>
+            <p>Street: ${brewery.street}</p>
+            <p>City: ${brewery.city}</p>
+            <p>Zipcode: ${brewery.postal_code}</p>
+            <button class="likes">
+                <i class="fas fa-beer"></i>
+                <span class="count">0</span>
+            </button>
+        </li>`;
+    }).join('');
+    profiles.innerHTML = htmlString
 };
 
-//Create event listener for search inputs that lets you search for breweries by city
-input.addEventListener('input', event => {
-    const searchStr = event.target.value.toLowerCase();
-    let searchResult = filteredBreweries.filter(brewery => {
-        return (brewery.city.toLowerCase().includes(searchStr));
-    });
-    displayBreweries(searchResult);
-});
 
-//Create a "like" button for each brewery
-const btn = document.createElement('button');
-document.body.append(btn);
-btn.className = "like";
+let filteredBreweries = [];
+fetchBreweries();
+
+
+function countLikes(){
+    // const ul = document.querySelector("ul")
+    profiles.addEventListener("click", e => {
+        e.preventDefault();
+        if (e.target.className == "count") {
+            return(e.target.textContent++);
+        };
+    });
+};
+countLikes();
+
