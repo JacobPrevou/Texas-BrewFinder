db99bae431ec07fbeb87c3d74a6fb3f665eaabbe
Jake <prevou15@gmail.com>
2021-12-30 14:50:51 -0600

    Actual final draft

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //
 
-//Create a div container for your search elements
+
 const container = document.createElement('div')
 document.body.append(container);
 container.className = "container";
 
-//Searchbar wrapper
+
 const searchWrapper = document.createElement('div');
 container.appendChild(searchWrapper);
 searchWrapper.id = "searchWrapper";
 
 
-//Create input box for searchBar
+
 const input = document.createElement('input');
 searchWrapper.appendChild(input);
 input.type = "text";
@@ -19,26 +19,12 @@ input.id = "searchBar";
 input.placeholder = "Type in a Texas city y'all!"
 
 
-//Create a list element for the brewery "profiles"
 const profiles = document.createElement('ul');
 container.append(profiles);
 profiles.id = "profiles";
 
 
-//Add an input event listener to search bar; search by city
-searchBar.addEventListener('input', e => {
-    e.preventDefault()
-    const searchString = e.target.value.toLowerCase();
-    let result = breweriesArr.filter((brewery) => {
-        return (
-            brewery.city.toLowerCase().includes(searchString)
-        );
-    });
-  loadBreweries(result);
-});
 
-
-//Ok you made the searchbar! Now access the API
 function fetchBreweries() {
     fetch("https://api.openbrewerydb.org/breweries?by_state=texas")
     .then(res => res.json())
@@ -67,21 +53,30 @@ const loadBreweries = (breweries) => {
     }).join('');
     profiles.innerHTML = site
 };
-//Call fetchBreweries
+
+searchBar.addEventListener('input', e => {
+    e.preventDefault()
+    const searchString = e.target.value.toLowerCase();
+    let result = breweriesArr.filter((brewery) => {
+        return (
+            brewery.city.toLowerCase().includes(searchString)
+        );
+    });
+  loadBreweries(result);
+});
+
 fetchBreweries();
 
 
-//Add an alert when you "like" a brewery ("Cheers!")
 profiles.addEventListener("click",function(e) {
     if(e.target && e.target.className == "fas fa-beer") {
         alert(" Cheers!");
     }
 });
 
-//Show "likes" by changing the color of the cheers button icon
+
 function showLikes(){
     profiles.addEventListener("click", e => {
-        e.preventDefault();
         if (e.target.className == "fas fa-beer") {
             return(e.target.style.color = "red");
         };
